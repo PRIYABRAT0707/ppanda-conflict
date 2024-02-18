3d8d2849217f50ec40c17c90e95cc2a1958a11db
PRIYABRAT PANDA <priyabrat.panda@eidiko-india.com>
2024-02-18 11:18:44 +0530

    Create Myfile2.jsx

---
+++ ppanda-react/src/Component/Myfile2.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Myfile2 = () => {
+  return (
+    <div>Myfile2
+
+
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Saepe vitae consectetur beatae vel, totam sapiente facilis repellendus non eos deserunt? Provident, architecto tempore ullam ex nostrum vitae dolore eum dignissimos.
+    </div>
+  )
+}
+
+export default Myfile2
