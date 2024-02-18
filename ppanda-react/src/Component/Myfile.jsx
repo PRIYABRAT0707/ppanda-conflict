882924d6970c35bd16dd5dc1fc77dc19406f70ba
PRIYABRAT PANDA <priyabrat.panda@eidiko-india.com>
2024-02-18 11:14:10 +0530

    jkvdjjvjdv

---
+++ ppanda-react/src/Component/Myfile.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Myfile = () => {
+  return (
+    <div>
+<h4>bcvvbnxzvcvnxnvcnxnvcnvm</h4>
+<input></input>
+<h1>jdshjdhjghgdhgvhdvgg</h1>
+
+    </div>
+  )
+}
+
+export default Myfile
